45e363ab4d066d539f025263078c6356fabb5543
Narendra Kumar <narendrapatel6321@gmail.com>
2026-03-14 16:41:00 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -111,7 +111,7 @@ def prepare_sbd(tar_path, extract_dir) -> None:
         shutil.rmtree(extract_dir)
 
     print(" Extracting SBD...")
-    with tarfile.open(tar_path, "r:gz") as tar:
+    with tarfile.open(tar_path, "r:*") as tar:
         tar.extractall(extract_dir.parent)
     sentinel.touch()
 
